975e206490216adf8c9580294ac752b13f475093
Peter-Andonov <peter.g.andonov@gmail.com>
2020-11-09 18:03:42 +0200

    added category browser component

--- bidster/app/admin.py
+++ bidster/app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from app.models import OfferCategory
+
+class OfferCategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(OfferCategory, OfferCategoryAdmin)
